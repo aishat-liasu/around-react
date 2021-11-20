ee84589ff965aa7c3be0e2a8b238b0d1b9aad184
arike-liasu <aishatliasu17965@gmail.com>
2021-11-20 06:27:11 +0100

    feat: move footer section to Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer class='footer'>
+      <p class='footer__copyright'>&copy; 2021 Around the U.S</p>
+    </footer>
+  );
+};
+
+export default Footer;
