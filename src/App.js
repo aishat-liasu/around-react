d37671ea0b801aa511dd6300b8099f3363ad61cb
arike-liasu <aishatliasu17965@gmail.com>
2021-11-20 06:28:28 +0100

    feat: import Header, Main and Foooter components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div class='page'>
@@ -139,52 +143,9 @@ function App() {
         </form>
       </section>
 
-      <header class='header'>
-        <div class='logo'></div>
-      </header>
-      <main class='main'>
-        <section class='profile'>
-          <div class='profile__content'>
-            <div class='profile__avatar'>
-              <div class='profile__overlay'></div>
-            </div>
-
-            <div class='profile__info'>
-              <div class='profile__title-edit'>
-                <h1 class='profile__title'>Jacques Cousteau</h1>
-                <button type='submit' class='profile__edit-button'></button>
-              </div>
-              <p class='profile__subtitle'>Explorer</p>
-            </div>
-          </div>
-
-          <button
-            type='submit'
-            class='profile__add-button'
-            aria-label='add'
-          ></button>
-        </section>
-        <section class='places'>
-          <template id='place-template'>
-            <div class='place'>
-              <button class='place__delete-button'></button>
-              <img
-                src='./images/latemar.png'
-                alt='place'
-                class='place__image'
-              />
-              <h2 class='place__title'>Title</h2>
-              <div class='place__love-section'>
-                <button class='place__love-button'></button>
-                <p class='place__love-count'>1</p>
-              </div>
-            </div>
-          </template>
-        </section>
-      </main>
-      <footer class='footer'>
-        <p class='footer__copyright'>&copy; 2021 Around the U.S</p>
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
