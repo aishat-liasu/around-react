8244e97088103ea94fb6112fea2b8278866c8d95
arike-liasu <aishatliasu17965@gmail.com>
2021-11-21 03:55:06 +0100

    refactor: change from 'class' to 'className'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer class='footer'>
-      <p class='footer__copyright'>&copy; 2021 Around the U.S</p>
+    <footer className='footer'>
+      <p className='footer__copyright'>&copy; 2021 Around the U.S</p>
     </footer>
   );
 };
