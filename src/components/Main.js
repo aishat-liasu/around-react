754b8daebd84e5b1a345bd6cc53515be80076539
arike-liasu <aishatliasu17965@gmail.com>
2021-11-21 03:53:51 +0100

    feat: update Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,41 +1,189 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import PopupWithForm from './PopupWithForm';
 
-const Main = () => {
+import api from '../utils/api';
+import PopupwithForm from './PopupWithForm';
+import PopupWithImage from './PopupWithImage';
+import Card from './Card';
+
+const Main = (props) => {
+  const [userName, setUserName] = useState('Jacques Cousteau');
+  const [userDescription, setUserDescription] = useState('Explorer');
+  const [userAvatar, setUserAvatar] = useState('');
+  //const [userId, setUserId] = useState('');
+  const userId = useRef('');
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    Promise.all([api.getUserInfo(), api.getInitialCards()]).then((result) => {
+      setUserName(result[0].name);
+      setUserDescription(result[0].about);
+      setUserAvatar(result[0].avatar);
+      userId.current = result[0]._id;
+      setCards(result[1]);
+    }) .catch((err) => {
+        console.log(err); // log the error to the console
+      };
+
+    return () => {
+
+    };
+  }, []);
+
   return (
-    <main class='main'>
-      <section class='profile'>
-        <div class='profile__content'>
-          <div class='profile__avatar'>
-            <div class='profile__overlay'></div>
+    <main className='main'>
+      <PopupwithForm
+        title='Edit Profile'
+        name='edit'
+        isOpen={props.isEditProfilePopupOpen}
+        onClose={props.onEditProfileClose}
+      >
+        <label className='popup__label'>
+          <input
+            type='text'
+            id='profile-title'
+            name='profileName'
+            className='popup__field popup__field_el_title'
+            minLength='2'
+            maxLength='45'
+            placeholder='Name'
+            required
+          />
+          <span className='popup__field-error' id='profile-title-error'></span>
+        </label>
+
+        <label className='popup__label'>
+          <input
+            type='text'
+            id='profile-subtitle'
+            name='profileJob'
+            className='popup__field popup__field_el_subtitle'
+            minLength='2'
+            maxLength='200'
+            placeholder='About'
+            required
+          />
+          <span
+            className='popup__field-error'
+            id='profile-subtitle-error'
+          ></span>
+        </label>
+      </PopupwithForm>
+
+      <PopupWithForm
+        title='New Place'
+        name='add'
+        isOpen={props.isAddPlacePopupOpen}
+        onClose={props.onAddPlaceClose}
+      >
+        <label className='popup__label'>
+          <input
+            type='text'
+            name='placeName'
+            id='place-title'
+            className='popup__field popup__field_el_place-title'
+            placeholder='Title'
+            minLength='1'
+            maxLength='30'
+            required
+          />
+          <span className='popup__field-error' id='place-title-error'></span>
+        </label>
+        <label className='popup__label'>
+          <input
+            type='url'
+            name='placeImageURL'
+            id='place-image-url'
+            className='popup__field popup__field_el_place-image-url'
+            placeholder='Image URL'
+            required
+          />
+          <span
+            className='popup__field-error'
+            id='place-image-url-error'
+          ></span>
+        </label>
+      </PopupWithForm>
+
+      <PopupWithForm
+        title='Update Profile Picture'
+        name='change'
+        isOpen={props.isEditAvatarPopupOpen}
+        onClose={props.onEditAvatarClose}
+      >
+        <label className='popup__label'>
+          <input
+            type='url'
+            name='profilePictureURL'
+            id='profile-picture-url'
+            className='popup__field popup__field_el_profile-picture-url'
+            placeholder='Image URL'
+            required
+          />
+          <span
+            className='popup__field-error'
+            id='profile-picture-url-error'
+          ></span>
+        </label>
+      </PopupWithForm>
+
+      <PopupWithForm
+        title='Are you sure?'
+        name='confirm'
+        isOpen={props.isDeletePlacePopupOpen}
+        onClose={props.onDeletePlaceClose}
+      ></PopupWithForm>
+      <PopupWithImage
+        cardData={props.selectedCard}
+        isOpen={props.isImagePopupOpen}
+        onClose={props.onImagePopupClose}
+      />
+
+      <section className='profile'>
+        <div className='profile__content'>
+          <div
+            className='profile__avatar'
+            style={{ backgroundImage: `url(${userAvatar})` }}
+          >
+            <div
+              className='profile__overlay'
+              onClick={props.onAvatarClick}
+            ></div>
           </div>
 
-          <div class='profile__info'>
-            <div class='profile__title-edit'>
-              <h1 class='profile__title'>Jacques Cousteau</h1>
-              <button type='submit' class='profile__edit-button'></button>
+          <div className='profile__info'>
+            <div className='profile__title-edit'>
+              <h1 className='profile__title'>{userName}</h1>
+              <button
+                type='submit'
+                className='profile__edit-button'
+                onClick={props.onEditProfileClick}
+              ></button>
             </div>
-            <p class='profile__subtitle'>Explorer</p>
+            <p className='profile__subtitle'>{userDescription}</p>
           </div>
         </div>
 
         <button
           type='submit'
-          class='profile__add-button'
+          className='profile__add-button'
           aria-label='add'
+          onClick={props.onAddPlaceClick}
         ></button>
       </section>
-      <section class='places'>
-        <template id='place-template'>
-          <div class='place'>
-            <button class='place__delete-button'></button>
-            <img src='./images/latemar.png' alt='place' class='place__image' />
-            <h2 class='place__title'>Title</h2>
-            <div class='place__love-section'>
-              <button class='place__love-button'></button>
-              <p class='place__love-count'>1</p>
-            </div>
-          </div>
-        </template>
+      <section className='places'>
+        {cards.map((item) => {
+          return (
+            <Card
+              userId={userId.current}
+              cardData={item}
+              key={item._id}
+              onCardClick={props.onCardClick}
+              onDeleteButtonClick={props.onDeletePlaceClick}
+            />
+          );
+        })}
       </section>
     </main>
   );
