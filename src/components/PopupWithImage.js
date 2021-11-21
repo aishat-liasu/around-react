01409a8f385428fd6a57643e9d3b1638192a17c0
arike-liasu <aishatliasu17965@gmail.com>
2021-11-21 03:51:54 +0100

    feat: update PopupWithImage component

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,38 +1,30 @@
-import Popup from './Popup.js';
-
 import React from 'react';
 
-const PopupWithImage = () => {
+const PopupWithImage = (props) => {
+  console.log(props.cardData);
   return (
-    <section className='popup popup_type_image' id='popup_type_image'>
+    <section
+      className={`popup popup_type_image ${
+        props.isOpen ? 'popup_opened' : ''
+      } `}
+      id='popup_type_image'
+    >
       <div className='popup__container'>
         <button
           type='reset'
           className='popup__close-button'
           aria-label='close'
+          onClick={props.onClose}
         ></button>
         <img
-          src="<%=require('./images/latemar.png')%>"
+          src={props.cardData.link || ''}
           alt='popup'
           className='popup__image'
         />
-        <p className='popup__image-location'></p>
+        <p className='popup__image-location'>{props.cardData.name}</p>
       </div>
     </section>
   );
 };
 
 export default PopupWithImage;
-
-/* export default class PopupWithImage extends Popup {
-  open({ name, link }) {
-    const popupImage = this._popupElement.querySelector(".popup__image");
-    const popupImageLocation = this._popupElement.querySelector(
-      ".popup__image-location"
-    );
-    popupImage.src = link;
-    popupImage.alt = `${name} picture`;
-    popupImageLocation.textContent = name;
-    super.open();
-  }
-} */
