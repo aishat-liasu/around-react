e663f6db7fa45058db4a186c13095ee863275282
arike-liasu <aishatliasu17965@gmail.com>
2021-11-22 23:25:22 +0100

    refactor: delete popups from Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-
 import api from '../utils/api';
-import PopupWithForm from './PopupWithForm';
-import ImagePopup from './ImagePopup';
 import Card from './Card';
 
 const Main = (props) => {
@@ -26,118 +23,10 @@ const Main = (props) => {
       .catch((err) => {
         console.log(err); // log the error to the console
       });
-    return () => {};
   }, []);
 
   return (
     <main className='main'>
-      <PopupWithForm
-        title='Edit Profile'
-        name='edit'
-        isOpen={props.isEditProfilePopupOpen}
-        onClose={props.onEditProfileClose}
-      >
-        <label className='popup__label'>
-          <input
-            type='text'
-            id='profile-title'
-            name='profileName'
-            className='popup__field popup__field_el_title'
-            minLength='2'
-            maxLength='45'
-            placeholder='Name'
-            required
-          />
-          <span className='popup__field-error' id='profile-title-error'></span>
-        </label>
-
-        <label className='popup__label'>
-          <input
-            type='text'
-            id='profile-subtitle'
-            name='profileJob'
-            className='popup__field popup__field_el_subtitle'
-            minLength='2'
-            maxLength='200'
-            placeholder='About'
-            required
-          />
-          <span
-            className='popup__field-error'
-            id='profile-subtitle-error'
-          ></span>
-        </label>
-      </PopupWithForm>
-
-      <PopupWithForm
-        title='New Place'
-        name='add'
-        isOpen={props.isAddPlacePopupOpen}
-        onClose={props.onAddPlaceClose}
-      >
-        <label className='popup__label'>
-          <input
-            type='text'
-            name='placeName'
-            id='place-title'
-            className='popup__field popup__field_el_place-title'
-            placeholder='Title'
-            minLength='1'
-            maxLength='30'
-            required
-          />
-          <span className='popup__field-error' id='place-title-error'></span>
-        </label>
-        <label className='popup__label'>
-          <input
-            type='url'
-            name='placeImageURL'
-            id='place-image-url'
-            className='popup__field popup__field_el_place-image-url'
-            placeholder='Image URL'
-            required
-          />
-          <span
-            className='popup__field-error'
-            id='place-image-url-error'
-          ></span>
-        </label>
-      </PopupWithForm>
-
-      <PopupWithForm
-        title='Update Profile Picture'
-        name='change'
-        isOpen={props.isEditAvatarPopupOpen}
-        onClose={props.onEditAvatarClose}
-      >
-        <label className='popup__label'>
-          <input
-            type='url'
-            name='profilePictureURL'
-            id='profile-picture-url'
-            className='popup__field popup__field_el_profile-picture-url'
-            placeholder='Image URL'
-            required
-          />
-          <span
-            className='popup__field-error'
-            id='profile-picture-url-error'
-          ></span>
-        </label>
-      </PopupWithForm>
-
-      <PopupWithForm
-        title='Are you sure?'
-        name='confirm'
-        isOpen={props.isDeletePlacePopupOpen}
-        onClose={props.onDeletePlaceClose}
-      ></PopupWithForm>
-      <ImagePopup
-        cardData={props.selectedCard}
-        isOpen={props.isImagePopupOpen}
-        onClose={props.onImagePopupClose}
-      />
-
       <section className='profile'>
         <div className='profile__content'>
           <div
