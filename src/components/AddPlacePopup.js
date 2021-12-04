00fb4b8c30effdf4dbb267f38c730d209a7024df
aishat-liasu <aishatliasu17965@gmail.com>
2021-12-04 20:53:45 +0100

    feat: create AddPlacePopup component

---
+++ src/components/AddPlacePopup.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import PopupWithForm from './PopupWithForm';
+
+const AddPlacePopup = ({ isOpen, onClose, onAddPlaceSubmit }) => {
+  const [name, setName] = useState('');
+  const [link, setLink] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    onAddPlaceSubmit({
+      name,
+      link,
+    });
+  }
+
+  return (
+    <PopupWithForm
+      title='New Place'
+      name='add'
+      isOpen={isOpen}
+      onClose={onClose}
+      buttonText='Save'
+      onSubmit={handleSubmit}
+    >
+      <label className='popup__label'>
+        <input
+          type='text'
+          name='placeName'
+          id='place-title'
+          className='popup__field popup__field_el_place-title'
+          placeholder='Title'
+          minLength='1'
+          maxLength='30'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <span className='popup__field-error' id='place-title-error'></span>
+      </label>
+      <label className='popup__label'>
+        <input
+          type='url'
+          name='placeImageURL'
+          id='place-image-url'
+          className='popup__field popup__field_el_place-image-url'
+          placeholder='Image URL'
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+          required
+        />
+        <span className='popup__field-error' id='place-image-url-error'></span>
+      </label>
+    </PopupWithForm>
+  );
+};
+
+export default AddPlacePopup;
