e92126b42a45c11740089f63e26d400e5d03ca93
aishat-liasu <aishatliasu17965@gmail.com>
2021-12-04 20:58:31 +0100

    refactor: update functions in Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,10 @@
-import React, { useState, useEffect, useContext } from 'react';
-import api from '../utils/api';
+import React, { useContext } from 'react';
+
 import Card from './Card';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 const Main = (props) => {
   const { name, about, avatar, _id } = useContext(CurrentUserContext);
-
-  const [cards, setCards] = useState([]);
-
-  console.log(name);
-
-  useEffect(() => {
-    api
-      .getInitialCards()
-      .then((result) => {
-        setCards(result);
-      })
-      .catch((err) => {
-        console.log(err); // log the error to the console
-      });
-  }, []);
-
-  function handleCardLike(card) {
-    // Check one more time if this card was already liked
-    const isLiked = card.likes.some((i) => i._id === _id);
-
-    // Send a request to the API and getting the updated card data
-    api.changeCardLikeStatus(card._id, isLiked).then((newCard) => {
-      console.log(newCard);
-      setCards((cards) => cards.map((c) => (c._id === card._id ? newCard : c)));
-    });
-  }
 
   return (
     <main className='main'>
@@ -67,14 +41,15 @@ const Main = (props) => {
         ></button>
       </section>
       <section className='places'>
-        {cards.map((item) => {
+        {props.cards.map((item) => {
           return (
             <Card
               cardData={item}
               key={item._id}
               onCardClick={props.onCardClick}
               onDeleteButtonClick={props.onDeletePlaceClick}
-              onCardLike={handleCardLike}
+              onCardLike={props.onCardLike}
+              onCardDelete={props.onCardDelete}
             />
           );
         })}
